c5935a93a8d1d49826286c15c295377c87e7c0e5
Ankur Gupta <rukna1000@gmail.com>
2018-10-02 22:41:53 -0400

    Verified that part 4 works

--- sort.c
+++ sort.c
@@ -64,9 +64,16 @@ void print_timeval(struct timeval t) {
 struct timeval timediff(struct timeval time1, struct timeval time2) {
   struct timeval diff;
 
-  // For now, set it to all zeroes
-  diff.tv_sec = 0L;
-  diff.tv_usec = 0L;
+	long int usecDiff = time2.tv_usec - time1.tv_usec;
+
+	if(usecDiff < 0) {
+		diff.tv_sec = time2.tv_sec - time1.tv_sec - 1;
+		diff.tv_usec = 1000000 + usecDiff;
+	} 
+	else {
+		diff.tv_sec = time2.tv_sec - time1.tv_sec;
+		diff.tv_usec = usecDiff;
+	}
 
   return diff;
 }
